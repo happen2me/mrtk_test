b394a2f342f8f4bc427d85dd64f4d99897598562
yuanchun <immr.shen@gmail.com>
2021-03-07 11:32:06 +0800

    first commit

---
+++ Assets/CustomScripts/ChangeImageOpacity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImageOpacity : MonoBehaviour
+{
+    RawImage image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<RawImage>();
+        // for (int i = 0; i < 4; i++)
+        // {
+        //     DecreaseOpacity();
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DecreaseOpacity()
+    {
+        Debug.Log("Image opacity decreasing");
+        var col = image.color;
+        col.a = col.a - 0.2f <= 0.0f ? 0.0f : col.a - 0.2f;
+        image.color = col;
+        Debug.Log("col.a: " + image.color.a);
+    }
+
+    public void IncreaseOpacity()
+    {
+        Debug.Log("Image opacity increasing");
+        var col = image.color;
+        col.a = col.a + 0.2f >= 1.0f ? 1.0f : col.a + 0.2f;
+        image.color = col;
+        Debug.Log("col.a: " + image.color.a);
+    }
+}
---
+++ Assets/CustomScripts/ChangeOpacity.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeOpacity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void decreaseOpacity()
+    {
+        Debug.Log("Opacity decreasing");
+        var childRenders = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (var render in childRenders)
+        {
+            var col = render.material.color;
+            col.a = col.a - 0.2f <= 0.0f ? 0.0f : col.a - 0.2f;
+            render.material.color = col;
+            Debug.Log("col.a: " + col.a);
+        }
+        Debug.Log("Opacity decreased by 0.2");
+        Debug.Log("childRenders len: " + childRenders.Length);
+        Debug.Log("last opacity: " + childRenders[0].material.color.a);
+    }
+
+    public void increaseOpacity()
+    {
+        var childRenders = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (var render in childRenders)
+        {
+            var col = render.material.color;
+            col.a = col.a + 0.2f >= 1.0f ? 1.0f : col.a + 0.2f;
+            render.material.color = col;
+        }
+        Debug.Log("Opacity increased by 0.2");
+    }
+}
