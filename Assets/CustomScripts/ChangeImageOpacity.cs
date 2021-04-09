20162c14bbb308f77f5f12ff1055b22746783c8a
yuanchun <immr.shen@gmail.com>
2021-04-09 13:35:52 +0800

    more pictures added

--- Assets/CustomScripts/ChangeImageOpacity.cs
+++ Assets/CustomScripts/ChangeImageOpacity.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class ChangeImageOpacity : MonoBehaviour
 {
     RawImage image;
+    public int image_num = 14;
+    private string image_prefix =  "Images/";
+    public string image_format = ".png";
+    private int cur_image_no = 1;
+    public float opacity_step = 0.25f;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +31,7 @@ public class ChangeImageOpacity : MonoBehaviour
     {
         Debug.Log("Image opacity decreasing");
         var col = image.color;
-        col.a = col.a - 0.2f <= 0.0f ? 0.0f : col.a - 0.2f;
+        col.a = col.a - opacity_step <= 0.0f ? 0.0f : col.a - opacity_step;
         image.color = col;
         Debug.Log("col.a: " + image.color.a);
     }
@@ -35,8 +40,35 @@ public class ChangeImageOpacity : MonoBehaviour
     {
         Debug.Log("Image opacity increasing");
         var col = image.color;
-        col.a = col.a + 0.2f >= 1.0f ? 1.0f : col.a + 0.2f;
+        col.a = col.a + opacity_step >= 1.0f ? 1.0f : col.a + opacity_step;
         image.color = col;
         Debug.Log("col.a: " + image.color.a);
+    }
+
+    public void NextImage(){
+        image.texture = newTexture(true);
+    }
+
+    public void PreviousImage(){
+        image.texture = newTexture(false);
+    }
+
+    private Texture2D newTexture(bool next)
+    {
+        if(next)
+        {
+            cur_image_no = cur_image_no % image_num + 1; // in range 1 to image_num
+        }
+        else
+        {
+            cur_image_no = cur_image_no==1 ? image_num : cur_image_no-1;
+        }
+        string next_texture_path = image_prefix + cur_image_no.ToString();
+        Debug.Log("next image path: " + next_texture_path);
+        Texture2D next_texture = Resources.Load<Texture2D>(next_texture_path);
+        if(next_texture is null){
+            Debug.Log("error loading image");
+        }
+        return next_texture;
     }
 }
---
+++ Assets/CustomScripts/KeyboardEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardEvent : MonoBehaviour
+{
+    public TouchScreenKeyboard keyboard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenSystemKeyboard()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.NumberPad, false, false, false, false);
+        Debug.Log("System keyboard opened");
+    }
+}
